55a907974a3964c2bc56f2c82c26c0271aa31205
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-09-20 11:23:08 +0200

    update

---
+++ lander/front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Simone
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<div>Hi There: I am the front-page.php template file.</div>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
