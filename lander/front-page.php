ad1a8cbc8e35e8a6f905919b6775e6f4b6a39b5f
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-09-21 14:55:28 +0200

    update

--- lander/front-page.php
+++ lander/front-page.php
@@ -1,24 +1,39 @@
-<?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package Simone
- */
-
+?php
+  /**
+* The template for displaying all pages.
+*
+* This is the template that displays all pages by default.
+* Please note that this is the WordPress construct of pages
+* and that other 'pages' on your WordPress site will use a
+* different template.
+*
+* @package Simone
+*/
 get_header(); ?>
-
 <div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+<main id="main" class="site-main" role="main">
+ 
+  
+<section id="call-to-action">
+  Call to Action
+</section><!-- #call-to-action -->
+  
+  
+<section id="services">
+  Services
+</section><!-- #services -->
 
 
+<section id="meet">
+  Meet
+</section><!-- #meet -->
 
-	</main><!-- #main -->
+
+</section><!-- #contact -->
+  Contact
+</section><!-- #contact -->
+
+</main><!-- #main -->
 </div><!-- #primary -->
-
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
