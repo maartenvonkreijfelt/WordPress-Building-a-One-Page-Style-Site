9e182c8ad2e38616f19712bda7bc48e9cd357550
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-09-25 10:07:41 +0200

    update

--- lander/front-page.php
+++ lander/front-page.php
@@ -39,6 +39,7 @@ get_header(); ?>
 	<div class="indent clear">
 		<?php 
 		$query = new WP_Query( 'pagename=services' );
+		$services_id = $query->queried_object->ID;
 
 		// The Loop
 		if ( $query->have_posts() ) {
@@ -53,9 +54,36 @@ get_header(); ?>
 
 		/* Restore original Post Data */
 		wp_reset_postdata();
+
+		$args = array(
+			'post_type' => 'page',
+			'post_parent' => $services_id //see in line 42 for the $service_id variable 
+		);
+		$services_query = new WP_Query( $args );
+		
+		// The Loop
+		if ( $services_query->have_posts() ) {
+			
+			echo '<ul class="services-list">';
+			while ( $services_query->have_posts() ) {
+				$services_query->the_post();
+				echo '<li class="clear">';
+				echo '<a href="' . get_permalink() . '" title="Learn more about ' . get_the_title() . '">';
+				echo '<h3 class="services-title">' . get_the_title() . '</h3>';
+				echo '</a>';
+				echo '<div class="services-lede">';
+				the_content();
+				echo '</div>';
+				echo '</li>';
+			}
+			echo '</ul>';
+		}
+
+		/* Restore original Post Data */
+		wp_reset_postdata();
 		?>
 	</div><!-- .indent -->
-</section><!-- #services -->
+</section>
 
 <section id="meet">
 	<div class="indent clear">
